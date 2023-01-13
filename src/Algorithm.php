c27015058b78eeeab5b4b6c2aea7e20fb4a0ad63
Adam Akbar <zainadam.id@gmail.com>
2023-01-13 12:23:15 +0700

    chore: add comment block

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -9,16 +9,24 @@ class Algorithm
 {
 	public static function eigenvector_centrality(Graph $graph)
 	{
+		// get the highest eigenvalues
 		$eigenvalueMax = max($graph->toMatrix()->eigenvalues());
+		
+		// get the number of node
 		$nNode = count($graph->nodes);
 
+		// get the matrix in array form
 		$arrayMatrix = $graph->toArrayMatrix();
 
+		// subtract the middle with the highest eigenvalues
 		for ($i = 0; $i < $nNode; $i++) {
 			$arrayMatrix[$i][$i] = $arrayMatrix[$i][$i] - $eigenvalueMax;
 		}
 
+		// create MathPHP matrix from new ArrayMatrix
 		$matrix = MatrixFactory::create($arrayMatrix);
+		
+		// $matrix->rref();
 		$ce = $matrix->rref()->getColumn($nNode - 1);
 
 		//sqrt transformation normalization;
@@ -26,6 +34,7 @@ class Algorithm
 			return pow($c, 2) ?: 1;
 		}, $ce)));
 
+        //normalize the score
 		foreach ($ce as &$c) {
 			$c = ($c ?: -1) * -1 / $rescaleValue;
 		}
